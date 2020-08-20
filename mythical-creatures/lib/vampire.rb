fac365d946942a304f28dae81a80fd2d857db0fb
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-20 17:11:27 -0600

    Passed vampire and dragon

--- mythical-creatures/lib/vampire.rb
+++ mythical-creatures/lib/vampire.rb
@@ -1,18 +1,20 @@
 class Vampire
 
-  attr_reader :name, :pet, :drink
+  attr_reader :name, :pet, :drink, :thirsty, :blood_count
   def initialize(name, pet = "bat")
     @name = name
     @pet = pet
+    @thirsty = true
+    @blood_count = 0
 
   end
 
   def thirsty?
-    true
-    false if drink
+    thirsty
   end
 
-  def false
-    truthy
+  def drink
+    @blood_count += 1
+    @thirsty = false if blood_count = 1
   end
 end
